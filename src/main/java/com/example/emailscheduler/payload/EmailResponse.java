d8f77a9dbaaf638944a394a22733a05f9eddd2eb
elxan <elxan310@gmail.com>
2022-02-21 17:38:01 +0400

    @Data annotation added into EmailResponse class

--- src/main/java/com/example/emailscheduler/payload/EmailResponse.java
+++ src/main/java/com/example/emailscheduler/payload/EmailResponse.java
@@ -1,11 +1,13 @@
 package com.example.emailscheduler.payload;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Data
 public class EmailResponse {
 
     @NonNull
